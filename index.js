2a4112a0807335cdd8f8521e13b9601d40d0b7b0
nct031194 <nct031194@icloud.com>
2023-03-07 23:05:39 +0700

    add index

---
+++ index.js
@@ -0,0 +1,6 @@
+var giatria = document.getElementById('a').value;
+var giatrib = document.getElementById('b').value;
+
+function Cong(){
+   document.getElementById('c').innerHTML(giatria + giatrib);
+}
